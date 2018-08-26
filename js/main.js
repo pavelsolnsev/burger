785a73bf28ba4720f4d7c87bdb8fc67aea812c58
pavelsolnsev <pavelsolnsev@gmail.com>
2018-08-26 21:21:37 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -21,8 +21,7 @@ for (let i = 0; i < accoItem.length; i++) {
         accoItem[i].classList.remove('menu-acco__item_active');
      
       }
-      accoItem[i].classList.add('menu-acco__item_active')
-   
+      accoItem[i].classList.add('menu-acco__item_active');
     }
   })
 }
@@ -41,3 +40,39 @@ for (let i = 0; i < accoItem.length; i++) {
         freeDrag: false,
         dots: false
 })
+
+
+// team accordeon
+const accoV = document.querySelector('.team');
+const accovItem = document.querySelectorAll('.team-acco__item');
+
+accoV.addEventListener('click', function(e) {
+  for (let i = 0; i < accovItem.length; i++) {
+    accovItem[i].classList.remove('team-acco__item_active');
+  }
+})
+
+for (let i = 0; i < accovItem.length; i++) {
+  accovItem[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    if (accovItem[i].classList.contains('team-acco__item_active')) {
+      accovItem[i].classList.remove('team-acco__item_active');
+     
+    } else {
+      for (let i = 0; i < accovItem.length; i++) {
+        accovItem[i].classList.remove('team-acco__item_active');
+     
+      }
+      accovItem[i].classList.add('team-acco__item_active');
+    }
+  })
+}
+
+ // Modal window
+
+    $('.button, .btn-footer').click(function (e) {
+        e.preventDefault();
+        $('#exampleModal').arcticmodal();
+    }); 
