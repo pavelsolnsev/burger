9cfd3a707749a3f80703185778575bc50572654b
pavelsolnsev <pavelsolnsev@gmail.com>
2018-08-29 21:35:47 +0300

    form

--- js/main.js
+++ js/main.js
@@ -331,7 +331,6 @@ function formatTime(time) {
   const minutes = Math.floor(roundTime / 60);
   const seconds = roundTime - minutes * 60;
   const formatedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-
   return minutes + ":" + formatedSeconds;
 }
 
